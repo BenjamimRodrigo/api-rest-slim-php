e71dc1124ab00b1098f0f4c76114cc4c6ced294e
Benjamim Rodrigo <soubenjamim@gmail.com>
2019-04-02 13:10:14 -0300

    Adicionado o código para a aplicação do charset UTF-8 na conexão com o banco de dados, corrigindo os erros nas consultas de dados com caracteres especiais.

--- models/Model.php
+++ models/Model.php
@@ -1,13 +1,11 @@
 <?php
 
 class Model {
-
 	public $PDO;
 	function __construct(){
-
 		 // Conexão com o banco de dados
-		$this->PDO = new \PDO('mysql:host=localhost;dbname=db_academia', 'root', '');
+		$this->PDO = new \PDO('mysql:host=localhost;dbname=db_academia', 'root', '', array('charset'=>'utf8'));
+		$this->PDO->query("SET CHARACTER SET utf8");
 		$this->PDO->setAttribute( \PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION );
 	}
-	
 }
